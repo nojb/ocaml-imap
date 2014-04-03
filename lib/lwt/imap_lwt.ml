9b88d7b413208ae5d64bcae35b75ea65acfd9b68
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2014-04-03 15:18:48 +0100

    Cleanup: add licence

--- lib/lwt/imap_lwt.ml
+++ lib/lwt/imap_lwt.ml
@@ -1,11 +1,24 @@
-(* module T = struct *)
-(* end *)
+(* The MIT License (MIT)
 
-(* module U : Imap_io_low.UNIX with module IO = T = struct *)
-(*   module IO = T *)
-(*   include Lwt_unix *)
-(*   let prerr_string = Lwt_io.eprint *)
-(* end *)
+   Copyright (c) 2014 Nicolas Ojeda Bar <n.oje.bar@gmail.com>
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in
+   all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE. *)
 
 let (>>=) = Lwt.bind
 
@@ -28,8 +41,6 @@ module Lwtio = struct
     Lwt.return buf
   let write = Lwt_io.write
   let flush = Lwt_io.flush
-  (* let close = Lwt_io.close *)
-  (* include Imap_io.Make (U) *)
 end
 
 include Imap.Make (Lwtio)
