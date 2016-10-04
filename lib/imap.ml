4a6ef332321372e1e9df5e3b70758b9c5a8882dd
Nicolas Ojeda Bar <n.oje.bar@gmail.com>
2016-10-04 09:39:30 +0200

    Implement Uint32Set.union

--- lib/imap.ml
+++ lib/imap.ml
@@ -75,12 +75,27 @@ module Uint32Set = struct
   type t = (int32 * int32) list (* disjoint, sorted intervals *)
   let empty = []
   let singleton n = [(n, n)]
-  let union s1 s2 =
-    assert false
+  let rec union s1 s2 =
+    let succ n = Int32.succ n in
+    let cmp n m = Uint32.compare n m in
+    let (<) n m = cmp n m < 0 in
+    let (<=) n m = cmp n m <= 0 in
+    let min n m = if n <= m then n else m in
+    let max n m = if n <= m then m else n in
+    let rec loop s1 s2 =
+      match s1, s2 with
+      | [], s2 -> s2
+      | s1, [] -> s1
+      | (a, b) :: x1, (c, d) :: x2 ->
+          if succ b < c then (a, b) :: loop x1 s2
+          else if succ d < a then (c, d) :: loop s1 x2
+          else union [min a c, max b d] (union x1 x2)
+    in
+    loop s1 s2
   let add n s =
-    assert false
+    union (singleton n) s
   let interval n m =
-    [(n, m)]
+    if n <= m then [n, m] else [m, n]
   let of_list l =
     List.fold_left (fun s n -> add n s) empty l
 end
