b152d040cb6c028cd9aa73cd6a042c6284b3e125
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2018-03-26 10:25:35 +0200

    Fix

--- lib/imap.ml
+++ lib/imap.ml
@@ -95,10 +95,7 @@ module Mutf7 = struct
     loop d e
 
   let replace s ch1 ch2 =
-    let s = Bytes.unsafe_of_string s in
-    for i = 0 to Bytes.length s - 1 do
-      if Bytes.get s i = ch1 then Bytes.set s i ch2
-    done
+    String.init (String.length s) (fun i -> if s.[i] = ch1 then ch2 else s.[i])
 
   let encode s =
     let b = Buffer.create 0 in
@@ -116,7 +113,7 @@ module Mutf7 = struct
         let str = String.sub s i (j - i) and buf = Buffer.create 32 in
         recode ~encoding:`UTF_8 `UTF_16BE (`String str) (`Buffer buf);
         let str = B64.encode ~pad:false (Buffer.contents buf) in
-        replace str '/' ',';
+        let str = replace str '/' ',' in
         Buffer.add_string b str; Buffer.add_char b '-'
       in
       let rec loop i =
@@ -154,7 +151,7 @@ module Mutf7 = struct
           match s.[i] with
           | '-' ->
               let str = String.sub s start (i - start) in
-              replace str ',' '/';
+              let str = replace str ',' '/' in
               let str = B64.decode str in (* FIXME do we need to pad it with "===" ? *)
               recode ~encoding:`UTF_16BE `UTF_8 (`String str) (`Buffer b);
               a (i + 1)
