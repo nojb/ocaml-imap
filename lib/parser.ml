cffe14c3943020570e497f226f0d30e6d8fcc1a1
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2018-07-09 22:37:09 +0200

    tagged responses

--- lib/parser.ml
+++ lib/parser.ml
@@ -50,9 +50,6 @@ let error buf =
 let sp buf =
   if curr buf = ' ' then next buf else error buf
 
-let resp_text_code buf =
-  error buf
-
 let take_while1 f buf =
   let pos0 = buf.pos in
   let pos = ref pos0 in
@@ -63,31 +60,77 @@ let take_while1 f buf =
   buf.pos <- !pos;
   String.sub buf.line pos0 (!pos - pos0)
 
+let is_atom_char = function
+  | '(' | ')' | '{' | ' '
+  | '\x00' .. '\x1F' | '\x7F'
+  | '%' | '*' | '"' | '\\' | ']' -> false
+  | '\x01' .. '\x7F' -> true
+  | _ -> false
+
+let atom =
+  take_while1 is_atom_char
+
+let is_astring_char c =
+  is_atom_char c || c = ']'
+
 let is_text_char = function
   | '\r' | '\n' -> false
   | '\x01' .. '\x7F' -> true
   | _ -> false
 
-let text buf k =
-  k (take_while1 is_text_char buf)
+let text buf =
+  take_while1 is_text_char buf
 
-let resp_text buf k =
+let resp_text_code buf =
+  error buf
+
+let resp_text buf =
   let code =
     match curr buf with
     | '[' -> Some (resp_text_code buf)
     | _ -> None
   in
-  text buf (k code)
+  code, text buf
+
+let is_tag_char = function
+  | '+' -> false
+  | c -> is_astring_char c
+
+let tag =
+  take_while1 is_tag_char
+
+let resp_cond_state buf =
+  let open Response.State in
+  match atom buf with
+  | "OK" ->
+      sp buf;
+      let code, text = resp_text buf in
+      OK (code, text)
+  | "NO" ->
+      sp buf;
+      let code, text = resp_text buf in
+      NO (code, text)
+  | "BAD" ->
+      sp buf;
+      let code, text = resp_text buf in
+      BAD (code, text)
+  | _ ->
+      error buf
 
 let response buf k =
   match curr buf with
   | '+' ->
       next buf;
       if curr buf = ' ' then next buf;
-      if not (is_eol buf) then resp_text buf (fun _ x -> k (Cont x))
+      if not (is_eol buf) then let _, x = resp_text buf in k (Cont x)
       else k (Cont "")
-  | _ ->
+  | '*' ->
       error buf
+  | _ ->
+      let tag = tag buf in
+      sp buf;
+      let state = resp_cond_state buf in
+      k (Tagged (tag, state))
 
 let parse s =
   let buf =
@@ -110,6 +153,19 @@ let%expect_test _ =
       "+";
       "+ Ready for literal data";
       "+ Ready for additional command text";
+      "abcd OK CAPABILITY completed";
+      "efgh OK STARTLS completed";
+      "ijkl OK CAPABILITY completed";
+      "a002 OK NOOP completed";
+      "a047 OK NOOP completed";
+      "A023 OK LOGOUT completed";
+      "a001 OK CAPABILITY completed";
+      "a002 OK Begin TLS negotiation now";
+      "a003 OK CAPABILITY completed";
+      "a004 OK LOGIN completed";
+      "A001 OK GSSAPI authentication successful";
+      "a001 OK LOGIN completed";
+      "A142 OK [READ-WRITE] SELECT completed";
     ]
   in
   List.iter parse tests;
