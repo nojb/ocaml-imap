1d8d9548975bf7e5bab7adbb31c8ec04fe3ee77b
Nicolás Ojeda Bär <n.oje.bar@gmail.com>
2018-07-20 21:17:28 +0200

    Expose only high-level interface

--- lib/imap.ml
+++ lib/imap.ml
@@ -20,38 +20,4 @@
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
    SOFTWARE. *)
 
-(** Non-blocking IMAP4 protocol codec.
-
-    [Imap] is a Lwt-compatible client library for the
-    {{:https://tools.ietf.org/html/rfc3501}IMAP4} protocol, together with some
-    {{!section:limitations}extensions}.
-
-    {3 References}
-    {ul
-    {- M. Crispin
-    {e {{:https://tools.ietf.org/html/rfc3501}INTERNET MESSAGE ACCESS PROTOCOL - VERSION 4rev1}, 2003}}}
-
-    {1:limitations Supported extensions and limitations}
-
-    The following extensions are supported:
-    - {{:https://tools.ietf.org/html/rfc4551}CONDSTORE}
-    - {{:https://tools.ietf.org/html/rfc5162}QRESYNC}
-    - {{:https://tools.ietf.org/html/rfc5161}ENABLE}
-    - {{:https://tools.ietf.org/html/rfc4315}UIDPLUS}
-    - {{:https://tools.ietf.org/html/rfc6154}SPECIAL-USE}
-    - {{:https://tools.ietf.org/html/rfc2177}IDLE}
-    - {{:https://developers.google.com/gmail/imap_extensions}X-GM-EXT-1} *)
-
-(** {1 Common types for IMAP} *)
-
-module MIME = MIME
-module Envelope = Envelope
-module Flag = Flag
-module Fetch = Fetch
-module Status = Status
-module Search = Search
-module Filter = Filter
-
-(** {3 Connections} *)
-
-include Core
+include Filter
